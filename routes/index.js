42688de430fecb446ce80d1fa53c58ce403528ae
dssa810 <dss.a810.hack@gmail.com>
2021-05-01 18:24:08 +0900

    cors정책 추가

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/ping', function(req, res, next) {
+  res.json(1);
+});
+
 module.exports = router;
