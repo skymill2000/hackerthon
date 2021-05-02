f73d3766725ea928d8c3ec417aade7c577187272
dssa810 <dss.a810.hack@gmail.com>
2021-05-02 15:03:06 +0900

    핫산님 도와주세요

--- services/api_test.js
+++ services/api_test.js
@@ -1,26 +1,12 @@
 let roitApi = require('./roit_api');
 let sampleData = require('./sample.json');
-
-const findUser = async () =>{
-    // console.log(sampleData.info.participants);
-    const findUser = "69kRXfbWSZ6wnnrVbvfbk20Lx6KFzFG1lH-t4I7k45m1Qredp_H4VLk-llvI-ccbbg4iWAhtdrAgOA"
-    const myData = sampleData.info.participants.find((element)=>{
-        if(element.puuid == findUser){
-            return element;
-        }
-    })
-    console.log(myData);
-}
+let smapleGamesData = require('../gameSamples.json');
 
 const main = async () => {
-    // let resultList = await roitApi.getUserMatch("그저그런녀석");
-    // console.log(resultList);
-    // for (const element of resultList) {
-    //     console.log(element.data);
-    //
-    // }
-
-    findUser();
+    let resultList = await roitApi.getUserMatch("그저그런녀석");
+    console.log(resultList);
+    // roitApi.JsontoFile(resultList); //샘플데이터 만들기
+    // findUser();
 }
 
 
--- services/roit_api.js
+++ services/roit_api.js
@@ -2,6 +2,9 @@ const axios = require('axios');
 const config = require('../config/apiConfig.json');
 const apikey = config.APIKEY;
 let puuid = "";
+const fs = require('fs');
+
+
 const getSummonerData = async (username) =>{
     let option = {
         method : 'GET',
@@ -16,6 +19,23 @@ const getSummonerData = async (username) =>{
     console.error(puuid);
     return userData.data;
 }
+
+const findUserData = async (myGameList) =>{
+    // console.log(sampleData.info.participants);
+    let onGameListResult = [];
+    const findUser = "69kRXfbWSZ6wnnrVbvfbk20Lx6KFzFG1lH-t4I7k45m1Qredp_H4VLk-llvI-ccbbg4iWAhtdrAgOA"
+    for (const myGame of myGameList) {
+        const myData = myGame.info.participants.find((element)=>{
+            if(element.puuid == findUser){
+                return element;
+            }
+        })
+        onGameListResult.push(myData);
+    }
+    console.log(onGameListResult);
+    return onGameListResult;
+}
+
 
 const getMatchV5 = async (puuid, start, count) =>{
     let url = 'https://asia.api.riotgames.com/lol/match/v5/matches/by-puuid/'+puuid+'/ids?start='+start+'&count='+count;
@@ -42,7 +62,7 @@ const getMatchDetail = async (matchId) => {
         }
     }
     const matchDetail = await axios(option);
-    return matchDetail;
+    return await matchDetail.data;
 }
 const getChampionData = () =>{
     axios.get('http://ddragon.leagueoflegends.com/cdn/11.9.1/data/en_US/champion.json').then((res)=>{
@@ -55,10 +75,12 @@ const getUserMatchData = async (name) =>{
         let returnList = [];
         const user = await getSummonerData(name);
         const matchList = await getMatchV5(user.puuid,0,10);
-        for (const match of matchList) {
+        for await (const match of matchList) {
             const matchDetail = await getMatchDetail(match);
             returnList.push(matchDetail);
         }
+        const userData = await findUserData(returnList);
+        console.log(userData);
         return returnList;
     }
     catch (e) {
@@ -66,11 +88,25 @@ const getUserMatchData = async (name) =>{
     }
 };
 
+const missionClear = () =>{
+
+}
+
+const JsontoFile = (list) => {
+    let data = JSON.stringify(list);
+    // fs.writeFileSync('../gameSamples.json', data);
+}
+
+const myOnGameData = async (gameList) => {
+    const myOnGames = await findUserData(gameList)
+}
+
 const missionCheck = (game) => {
 
 }
 
 module.exports = {
     getUserMatch : getUserMatchData,
-    getChampionData : getChampionData
+    getChampionData : getChampionData,
+    JsontoFile : JsontoFile,
 }
